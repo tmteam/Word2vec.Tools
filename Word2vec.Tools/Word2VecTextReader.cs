cf5bd4075921c12168ffd9f3695951a6adf662bb
tmteam <tmteam@yandex.ru>
2016-10-20 03:36:48 +0400

    Improved Word2VecTextReader interface
    Using StreamReader as one of possible sources

--- Word2vec.Tools/Word2VecTextReader.cs
+++ Word2vec.Tools/Word2VecTextReader.cs
@@ -11,59 +11,72 @@ namespace Word2vec.Tools
     /// </summary>
     public class Word2VecTextReader: IWord2VecReader
     {
+        public Vocabulary Read(StreamReader inputStream)
+        {
+            if (inputStream == null)
+                throw new ArgumentNullException(nameof(inputStream));
+            bool isFirstLine = true;
+            int vectorSize = -1;
+
+            var vectors = new List<WordRepresentation>();
+
+            var enUsCulture = CultureInfo.GetCultureInfo("en-US");
+            while (!inputStream.EndOfStream)
+            {
+                var line = inputStream.ReadLine().Split(' ');
+                if (isFirstLine)
+                {
+                    if (line.Length == 2)
+                    {
+                        var vocabularySize = 0;
+                        try
+                        {
+                            //header
+                            vocabularySize = int.Parse(line[0]);
+                            vectorSize = int.Parse(line[1]);
+                            vectors = new List<WordRepresentation>(vocabularySize);
+                            continue;
+                        }
+                        catch
+                        {
+                            vocabularySize = 0;
+                            vectorSize = -1;
+                        }
+                    }
+
+                    if (vectorSize == -1)
+                    {
+                        // no header, so require all other vectors to match first line's length
+                        vectorSize = line.Length - 1;
+                    }
+
+                    isFirstLine = false;
+                }
+
+                var vecs = line.Skip(1).Take(vectorSize).ToArray();
+                if (vecs.Length != vectorSize)
+                    throw new FormatException("word \"" + line.First() + "\" has wrong vector size of " + vecs.Length);
+
+                vectors.Add(new WordRepresentation(
+                   word: line.First(),
+                   vector: vecs.Select(v => Single.Parse(v, enUsCulture)).ToArray()));
+            }
+            return new Vocabulary(vectors, vectorSize);
+        }
         public Vocabulary Read(System.IO.Stream inputStream)
         {
             using (var strStream = new System.IO.StreamReader(inputStream))
             {
-                bool isFirstLine = true;
-                int vocabularySize = 0;
-                int vectorSize = -1;
-
-                //var firstLine = strStream.ReadLine().Split(' ');
-
-                var vectors = new List<WordRepresentation>();
-
-                var enUsCulture = CultureInfo.GetCultureInfo("en-US");
-                while (!strStream.EndOfStream)
-                {
-                    var line = strStream.ReadLine().Split(' ');
-                    if (isFirstLine) {
-                        if (line.Length == 2) {
-                            try {
-                                //header
-                                vocabularySize = int.Parse(line[0]);
-                                vectorSize = int.Parse(line[1]);
-                                vectors = new List<WordRepresentation>(vocabularySize);
-                                continue; 
-                            } catch {
-                                vocabularySize = 0;
-                                vectorSize = -1;
-                            }
-                        }
-
-                        if (vectorSize == -1) {
-                            // no header, so require all other vectors to match first line's length
-                            vectorSize = line.Length - 1;
-                        }
-
-                        isFirstLine = false;
-                    }
-
-                    var vecs = line.Skip(1).Take(vectorSize).ToArray();
-                    if (vecs.Length != vectorSize)
-                        throw new FormatException("word \"" + line.First() + "\" has wrong vector size of " + vecs.Length);
-                    
-                    vectors.Add(new WordRepresentation(
-                       word: line.First(),
-                       vector: vecs.Select(v => Single.Parse(v, enUsCulture)).ToArray()));
-                }
-                return new Vocabulary(vectors, vectorSize);
+                return Read(strStream);
             }
         }
 
         public Vocabulary Read(string path)
         {
-            return Read(new FileStream(path, FileMode.Open));
+            using (var fileSteram = new FileStream(path, FileMode.Open))
+            {
+               return Read(fileSteram);
+            }
         }
     }
 }
